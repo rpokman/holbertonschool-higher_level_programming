eaab44c0a7f2fe09116674c3bc5d13c6fb1e650e
rpokman <roymathis74200@gmail.com>
2025-09-02 13:28:15 +0200

    Fix print_last_digit function to correctly calculate the last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    numberlast = -(abs(number) % 10)
-    numberlast = number % 10
+    numberlast = (abs(number) % 10)
     print("{}".format(numberlast), end="")
     return numberlast
