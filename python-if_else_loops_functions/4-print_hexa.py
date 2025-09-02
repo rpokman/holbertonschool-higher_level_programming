0c14371ef0cdcd5940d93f4542d57d5b33b03495
rpokman <roymathis74200@gmail.com>
2025-09-02 12:02:45 +0200

    Refactor 4-print_hexa.py to print letters a-z excluding e and q; update 5-print_comb2.py for consistent formatting

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for a in range(0, 99):
-    print("{} = 0x{:x}".format(a, a))
+for letter in range(97, 123):
+    if letter != 101 and letter != 113:
+        print("{}".format(chr(letter)), end="")
