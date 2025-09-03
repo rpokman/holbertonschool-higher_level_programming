7722bef84e00c01446f283f7af150e0402217fdc
rpokman <roymathis74200@gmail.com>
2025-09-03 10:24:18 +0200

    Refactor fizzbuzz function to simplify range initialization

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    a = 1
-    for a in range (101):
+    for a in range (1, 101):
         if a % 3 == 0 and a % 5 == 0:
             print("FizzBuzz", end=" ")
         elif a % 3 == 0:
