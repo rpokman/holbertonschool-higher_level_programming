4980b05a194c9230a8a97abdf6a93a39ea78a041
rpokman <roymathis74200@gmail.com>
2025-09-08 23:50:33 +0200

    Implement square_matrix_simple function to compute the square of each element in a matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:  
+        new_row = []
+        for i in range(len(row)):  
+            new_row.append(row[i] ** 2)
+        new_matrix.append(new_row)
+
+    return new_matrix
