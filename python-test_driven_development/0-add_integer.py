69e25747c08432bdef1774644efc63e0a7e94558
rpokman <roymathis74200@gmail.com>
2025-09-12 09:38:20 +0200

    Refactor add_integer function to enhance docstring and add type checks; update test cases for improved error handling

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
+"""
+Module 0-add_integer
+Contient une fonction pour additionner deux entiers
+"""
 def add_integer(a, b=98):
+    """
+    Adds two integers.
+    Args:
+        a: First number (integer or float)
+        b: Second number (integer or float), defaults to 98
+    Returns:
+        Integer: The sum of a and b converted to integers
+    Raises:
+        TypeError: If a or b is not an integer or float
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
