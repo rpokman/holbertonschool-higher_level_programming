c4ca4f4adcc8dfef8ebc1df38c9a6be6f5f92d30
rpokman <roymathis74200@gmail.com>
2025-09-12 09:58:14 +0200

    Refactor add_integer function and tests: improve docstring clarity and ensure newline at end of test file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """
 Module 0-add_integer
-Contains a function that add two integers
+Contains a function that adds two integers.
 """
+
+
 def add_integer(a, b=98):
     """
-    Adds two integers.
+    Add two integers.
+
     Args:
         a (int or float): The first number.
         b (int or float): The second number, defaults to 98.
+
     Returns:
         int: The integer sum of a and b.
+
     Raises:
         TypeError: If a or b is not an integer or float.
     """
@@ -18,14 +23,13 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     if isinstance(a, float):
-        if a != a:
+        if a != a or a in [float('inf'), float('-inf')]:
             raise TypeError("a must be an integer")
-        if a in [float('inf'), float('-inf')]:
-            raise TypeError("a must be an integer")
+
     if isinstance(b, float):
-        if b != b:
+        if b != b or b in [float('inf'), float('-inf')]:
             raise TypeError("b must be an integer")
-        if b in [float('inf'), float('-inf')]:
-            raise TypeError("b must be an integer")
+
     return int(a) + int(b)
