64a1817ce667d79774a116d8a0cc92c8fed2fc07
rpokman <roymathis74200@gmail.com>
2025-09-15 10:24:42 +0200

    Implement Square class and add main script for testing

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 1-square
+Defines a class Square with a private size attribute.
+"""
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square instance
+        Args:
+            size: size of the square (no type/value verification at this stage)
+        """
+        self.__size = size
