0fc64dcf574338629385ecfa3d268da3cfa4a691
rpokman <roymathis74200@gmail.com>
2025-09-15 11:22:59 +0200

    Implement Square class with validation and add main script for testing

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module 2-square
+Defines a class Square with a private size attribute and validation.
+"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square instance
+        Args:
+            size (int): size of the square
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size ** 2
+    
