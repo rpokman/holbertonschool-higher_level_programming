50703b0d1f7ab0e2c9919f05a52d39917ceca5bf
rpokman <roymathis74200@gmail.com>
2025-09-16 10:24:10 +0200

    Fix import statements in main files to use quotes for module names

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,40 @@
 #!/usr/bin/python3
+"""
+Module 0-rectangle
+Defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+    Currently an empty class (to be implemented later).
+    """
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle instance."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
--- python-more_classes/8-main.py
+++ python-more_classes/8-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Rectangle = __import__(8-rectangle).Rectangle
+Rectangle = __import__("8-rectangle").Rectangle
 
 my_rectangle_1 = Rectangle(8, 4)
 my_rectangle_2 = Rectangle(2, 3)
