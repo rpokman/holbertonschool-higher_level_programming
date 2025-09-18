04c6bdb3f86e922b39523c6e811d2da77d43da88
rpokman <roymathis74200@gmail.com>
2025-09-18 09:56:28 +0200

    Fix typo in module docstring for clarity

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+"""This module define a functions that retrieve atribute and method of an object"""
 def lookup(obj):
     """returns the list of available attributes and methods of an object"""
     return dir(obj)
