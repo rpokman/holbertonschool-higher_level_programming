d90c4a3d70950041c41b79332fddf80a737fe183
rpokman <roymathis74200@gmail.com>
2025-09-29 11:52:18 +0200

    Implement functionality to add command line arguments to a list and save to a JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""Module that adds all arguments to a Python list, and then saves them to a file"""
+import sys
+from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+if exists(filename):
+    try:
+        items = load_from_json_file(filename)
+    except Exception:
+        items = []
+else:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
