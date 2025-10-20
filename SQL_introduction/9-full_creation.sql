284dbfb53b767e5b78d0cc164ebc4cb98e7fd2be
rpokman <roymathis74200@gmail.com>
2025-10-20 13:29:12 +0200

    Fix SQL syntax by adding a missing comma in second_table definition

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Create table and add multiples rows
 CREATE TABLE IF NOT EXISTS second_table(
   id INT,
-  name VARCHAR(256)
+  name VARCHAR(256),
   score INT
 );
 INSERT INTO second_table (id, name, score) VALUES
