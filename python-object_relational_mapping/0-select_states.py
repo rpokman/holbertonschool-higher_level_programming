32adad50b04a3c7ea546b4fb48ffc81465e51493
rpokman <roymathis74200@gmail.com>
2025-10-23 12:06:39 +0200

    Refactor 0-select_states.py for improved readability and consistency in variable naming

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,17 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], host="localhost", port=3306,
-                         charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cur.fetchall():
+    db = MySQLdb.connect(
+        port=3306,
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
         print(row)
-    cur.close()
+    cursor.close()
     db.close()
