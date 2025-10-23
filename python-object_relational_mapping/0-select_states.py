cde9eea977015e6b01f5090cffa80b68dc7e2bed
rpokman <roymathis74200@gmail.com>
2025-10-23 11:58:31 +0200

    Refactor 0-select_states.py to improve database connection and remove unnecessary comments; add 1-filter_states.py as a new file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
-
 import MySQLdb
 import sys
 
@@ -8,18 +6,10 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
     cursor = db.cursor()
-
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    rows = cursor.fetchall()
-
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
-
     cursor.close()
     db.close()
