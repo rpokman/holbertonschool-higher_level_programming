4b9217abc0c223d8651c24184302e9f53603f08d
rpokman <roymathis74200@gmail.com>
2025-10-23 12:06:08 +0200

    Refactor 0-select_states.py to simplify database connection and improve code clarity

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py username password database_name
-"""
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        port=3306,
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-        )
-    # Create a cursor object
-    cursor = db.cursor()
-    # Execute the SQL query to fetch all states ordered by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    # Fetch all results
-    query_rows = cursor.fetchall()
-    # Print each row (state)
-    for row in query_rows:
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], host="localhost", port=3306,
+                         charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
         print(row)
-    # Close the cursor and database connection
-    cursor.close()
+    cur.close()
     db.close()
