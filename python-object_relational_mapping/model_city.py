03540942e76784c0cc0cfcc307e91247069b5c35
rpokman <roymathis74200@gmail.com>
2025-10-24 16:33:24 +0200

    Refactor 14-model_city_fetch_by_state.py to improve docstring and query logic; update model_city.py class definition and foreign key reference

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Model definition of a City class for SQLAlchemy."""
+"""module that contains the class definition of a City"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """Class definition of a City."""
+    """City class mapped to 'cities' table"""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('state.id'), nullable=False)
