88eefdb35a60e50bdf717c1f256e71c37bb13c30
rpokman <roymathis74200@gmail.com>
2025-10-24 15:16:17 +0200

    Add 4-cities_by_state.py to list all cities from the database hbtn_0e_4_usa

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Takes in arguments and displays all values safe from MySQL injection."""
+"""Lists all cities from the database hbtn_0e_4_usa."""
 import sys
 import MySQLdb
 
@@ -12,9 +12,9 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     c = db.cursor()
-    state_name = sys.argv[4]
-    c.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-              (state_name,))
+    c.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                 JOIN states ON cities.state_id = states.id
+                 ORDER BY cities.id ASC""")
     for row in c.fetchall():
         print(row)
     c.close()
