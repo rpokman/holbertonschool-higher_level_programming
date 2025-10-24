3eeefbf632030b436c718c2a4172176771240f98
rpokman <roymathis74200@gmail.com>
2025-10-24 16:34:48 +0200

    Refactor 4-cities_by_state.py to improve docstring clarity, standardize host address, and enhance query execution

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa."""
-import sys
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="127.0.0.1",
         port=3306,
+        host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
+        )
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
     )
-    c = db.cursor()
-    c.execute("""SELECT cities.id, cities.name, states.name FROM cities
-                 JOIN states ON cities.state_id = states.id
-                 ORDER BY cities.id ASC""")
-    for row in c.fetchall():
+    query_rows = cursor.fetchall()
+    for row in query_rows:
         print(row)
-    c.close()
+    cursor.close()
     db.close()
