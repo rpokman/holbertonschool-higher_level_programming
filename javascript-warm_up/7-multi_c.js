94af040dbf62cdeef048adc9f2acdd6551b5cdf8
rpokman <roymathis74200@gmail.com>
2025-11-07 16:59:14 +0100

    Refactor 7-multi_c.js to improve argument handling and streamline output logic

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const argument = process.argv;
-const string = 'C is fun';
-let i = 0;
-for (i; i < argument[2]; i++) {
-  console.log(string);
+const x = parseInt(process.argv[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
